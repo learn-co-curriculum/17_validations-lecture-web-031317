87b1d25c72da1996563f704818d2946222580883
ipc103 <iancandysemail@gmail.com>
2017-04-12 16:48:59 -0400

    post lecture

--- student_lister/app/controllers/courses_controller.rb
+++ student_lister/app/controllers/courses_controller.rb
@@ -4,17 +4,28 @@ class CoursesController < ApplicationController
     @courses = Course.all
   end
 
+  def show
+    @course = Course.find(params[:id])
+  end
+
   def new
     @course = Course.new
   end
 
-  def edit
-    @course = Course.find(params[:id])
+  def create
+    # if the course is able to be saved
+      @course = Course.new(course_params)
+      if @course.save
+        redirect_to courses_path
+      else
+        # if it's not, we should show the user the new view again
+        render 'new'
+      end
+    # and display all of the error message that we have
   end
 
-  def create
-    course = Course.create(course_params)
-    redirect_to courses_path
+  def edit
+    @course = Course.find(params[:id])
   end
 
   private
--- student_lister/app/models/course.rb
+++ student_lister/app/models/course.rb
@@ -1,9 +1,20 @@
 class Course < ApplicationRecord
-  validates :name, presence: true
-
+  validates :name, presence: true, uniqueness: true
+  validates :description, presence: true, format: {without: /\Adog\z/i, message: "don't just make it dog!!!!"}
+  # validates :description, presence: true, exclusion: {in: ['dog', "Dog"] }
+  # validates :description, presence: true
+  # validate :description_isnt_dog
 
   # A course is a valid if it has a unique name
   # and if it has a description
 
   # and if that description is not the word 'dog'
+
+  # private
+  #
+  # def description_isnt_dog
+  #   if description && description.downcase == 'dog'
+  #     errors.add(:description, "shouldn't just be dog")
+  #   end
+  # end
 end
